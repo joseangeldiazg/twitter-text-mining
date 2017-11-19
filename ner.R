a7f7072a3174237d08cd103398eb9a8abbfffef9
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-11-19 18:25:27 +0100

    Añadido FP-Growth. Funciona mal.

--- ner.R
+++ ner.R
@@ -182,7 +182,13 @@ finalCorpus <- tm_map(finalCorpus, removeWords, myStopwords)
 # Borramos los espacios extra
 finalCorpus <- tm_map(finalCorpus, stripWhitespace)
 
-(finalCorpus$content[1])
+
+#*************************************************
+#REGLAS DE ASOCIACION
+#*************************************************
+
+
+#Vamos a probar primero con Apriori, ya que es exahustivo
 
 #*************************************************
 #Creamos las transacciones para las reglas de asociación
@@ -193,14 +199,68 @@ transactions <- as(items, "transactions")
 
 help(apriori)
 
-itemsets <- apriori(transactions, parameter = list(sup = 0.1, conf = 0.1, target="frequent itemsets",minlen=1))
+itemsets <- apriori(transactions, parameter = list(sup = 0.0001, conf = 0.1, target="frequent itemsets",minlen=1))
 inspect(itemsets)
 
 
-rules <- apriori(transactions, parameter = list(sup = 0.1, conf = 0.1, target="rules",minlen=1))
+rules <- apriori(transactions, parameter = list(sup = 0.0001, conf = 0.7, target="rules",minlen=1))
 inspect(rules)
 
 
+top.confidence <- sort(rules, decreasing = TRUE, na.last = NA, by = "confidence")
+inspect(head(top.confidence, 10))
 
-# TODO: Usar FPgrowth para reglas de asociación. 
-# TODO: ¿Posible enfoque con clustering jerarquico?
+
+#Apriori es muy lento, vamos a usar una aproximación por Spark para ver si funciona mejor
+
+
+#*************************************************
+# FP-GROWTH - SPARK FRAMEWORK
+#*************************************************
+
+
+#Iniciamos sesión en Spark
+
+if (nchar(Sys.getenv("SPARK_HOME")) < 1) {
+  Sys.setenv(SPARK_HOME = "/Users/joseadiazg/spark-2.2.0-bin-hadoop2.7")
+}
+
+library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
+sparkR.session(master = "local[*]", sparkConfig = list(spark.driver.memory = "6g"))
+
+
+#Creamos un dataset para que FP-GROWTH pueda entenderlo
+
+#No podemos tener items repetidos en una transaccion, por lo que haremos una nueva versión
+
+
+items <- strsplit(as.character(finalCorpus$content), " ")
+
+reduce_row = function(i) {
+  split = strsplit(i, split=" ")
+  paste(unique(split[[1]]), collapse = " ") 
+}
+
+itemsUnique<-lapply(finalCorpus$content[1:length(finalCorpus$content)],reduce_row)
+
+
+
+
+
+
+
+dfspark <- selectExpr(createDataFrame(data.frame(rawItems = as.character(itemsUnique)
+)), "split(rawItems, ' ') AS items")
+
+fpm <- spark.fpGrowth(dfspark, itemsCol="items", minSupport=0.00000000000001, minConfidence=0.1)
+
+# Extracting frequent itemsets
+
+spark.freqItemsets(fpm)
+
+# Extracting association rules
+
+spark.associationRules(fpm)
+
+
+
